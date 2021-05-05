cebdb022b96ce44d5fbb1ab1a5822d889e532546
didi <didi@localhost.localdomain>
2021-05-06 00:02:01 +0200

    vagrant

--- vagrant/parte_final/install.sh
+++ vagrant/parte_final/install.sh
@@ -3,10 +3,10 @@ sudo dnf -y install openldap-servers openldap-clients
 sudo ulimit -n 1024
 sudo rm -rf /etc/openldap/slapd.d/*
 sudo rm -rf /var/lib/ldap/*
-sudo cp /opt/docker/DB_CONFIG /var/lib/ldap/.
-sudo slaptest -f /opt/docker/slapd.conf -F /etc/openldap/slapd.d
-sudo slapadd -F /etc/openldap/slapd.d -l /opt/docker/edt.org.ldif
-sudo chown -R ldap.ldap /etc/openldap/slapd.d
-sudo chown -R ldap.ldap /var/lib/ldap
-sudo cp /opt/docker/ldap.conf /etc/openldap/ldap.conf
-sudo /sbin/slapd -d0
+sudo cp /vagrant/ldap/DB_CONFIG /var/lib/ldap/.
+sudo slaptest -f /vagrant/ldap/slapd.conf -F /etc/openldap/slapd.d
+#sudo slapadd -F /etc/openldap/slapd.d -l /vagrant/ldap/edt.org.ldif
+#sudo chown -R ldap.ldap /etc/openldap/slapd.d
+#sudo chown -R ldap.ldap /var/lib/ldap
+#sudo cp /vagrant/ldap/ldap.conf /etc/openldap/ldap.conf
+#sudo /sbin/slapd -d0
